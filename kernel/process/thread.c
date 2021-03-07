49613b1c12a2f6da0f02dc61cdc818ed9776089c
Song Liantao <songliantao@Songs-MacBook-Pro.local>
2021-03-07 10:57:50 +0800

    do lab2

--- kernel/process/thread.c
+++ kernel/process/thread.c
@@ -161,6 +161,11 @@ static u64 load_binary(struct process *process,
 			 * page aligned segment size. Take care of the page alignment when allocating
 			 * and mapping physical memory.
 			 */
+			
+			
+			seg_sz = elf->p_headers[i].p_memsz;
+			seg_map_sz = ROUND_UP(seg_sz, PAGE_SIZE);
+			
 
 			pmo = obj_alloc(TYPE_PMO, sizeof(*pmo));
 			if (!pmo) {
@@ -179,6 +184,10 @@ static u64 load_binary(struct process *process,
 			 * You should copy data from the elf into the physical memory in pmo.
 			 * The physical address of a pmo can be get from pmo->start.
 			 */
+			
+			
+			mem_copy(pmo->start, elf->p_headers[i].p_vaddr, seg_map_sz);
+			
 
 			flags = PFLAGS2VMRFLAGS(elf->p_headers[i].p_flags);
 
